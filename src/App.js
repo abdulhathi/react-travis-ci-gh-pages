049fb3da7990ff3bb4a4c150bdfe50ebddb391be
Abdul Hathi Mohamed Hussain <abdulhathi@gmail.com>
2024-04-03 18:23:28 -0700

    Changing the greetings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Assalamu Alaikum</h1>
+        <h1>Assalamu Alaikum !!!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -20,7 +20,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
